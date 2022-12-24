0e5810da79728be81bb9de213e1ed0d4b7f9947b
Felipe Tommaselli <f.tommaselli@usp.br>
2022-12-24 15:53:47 -0300

    convertendo o código para orientação em classes e objetos, reestruturando a organização geral

--- src/lidar2images.py
+++ src/lidar2images.py
@@ -33,59 +33,64 @@ import math
 import numpy as np
 import os
 
-# move from root (\src) to \assets\tags
-os.chdir('..') 
-path = os.getcwd() + '\\datasets\\'
-data_file_name = os.path.join(path,'syncro_data_validation.csv')
+class lidar2images:
 
-#filedata = open('/home/andres/Documents/learning_lidar/syncro_data.csv',"r")
-#finalData = open('/home/andres/Documents/learning_lidar/filter_syncro_data_validation.csv',"w")
-#finalData = open('/home/andres/Documents/learning_lidar/filter_syncro_data.csv',"w")
-#finalData = open('/home/andres/Documents/learning_lidar/filter_syncro_data_norm.csv',"w")
-#finalData = open('/home/andres/Documents/learning_lidar/filter_syncro_data_validation_norm.csv',"w")
-filedata = open(data_file_name,"r")
-data = filedata.readlines()
-
-def limitLidar(readings):
-    final_readings= []
-    for t in range(0,len(readings)):
-            final_readings.append(int(readings[t])/1000)
-    #print(readings)
-    return final_readings #, maxval
-
-def polar2xy(lidar):
-        x_lidar = []
-        y_lidar = []
-        min_angle = np.deg2rad(-45)
-        max_angle = np.deg2rad(225)
-        angle = np.linspace(min_angle, max_angle, 1081, endpoint = False)
-        for i in range(0, len(lidar)):
-            x_lidar.append(lidar[i]*math.cos(angle[i]))
-            y_lidar.append(lidar[i]*math.sin(angle[i]))
-            
-        return x_lidar, y_lidar
-
-def plot_lines(xl,yl,t):
-    LW=0.8
-    plt.cla()
-    plt.plot(xl,yl,'g.')
-    plt.xlim(-LW,LW)
-    plt.ylim(-1,5)
-    plt.axis('off')
-    plt.pause(0.1)
-    print(t)
-    # name = '/home/andres/Documents/learning_lidar/images_approach/images/image'+str(t)
-    #plt.savefig(name)
-    plt.savefig(os.getcwd() + '\\assets\\images' + '\\image' + str(t))
+    def __init__(self):
+        self.data = self.getData()
 
 
-for step in range(0,len(data)):
-    lidar = (data[step].split(","))[7:1088]
-    #print(lidar)
-    lidar_readings = limitLidar(lidar)
-    #print(lidar_readings)
-    x,y = polar2xy(lidar_readings) 
-    #print(x)
-    plot_lines(x,y,step)
+    def getData(self):
+        # move from root (\src) to \assets\tags
+        os.chdir('..') 
+        path = os.getcwd() + '\\datasets\\'
+        s = 'syncro_data_validation.csv'
+        # s = filter_syncro_data_validation.csv
+        data_file_name = os.path.join(path, s)
 
-filedata.close()
+        filedata = open(data_file_name,"r")
+        data = filedata.readlines()
+        filedata.close()
+        return data
+
+    def limitLidar(self, readings):
+        final_readings= []
+        for t in range(0,len(readings)):
+                final_readings.append(int(readings[t])/1000)
+        #print(readings)
+        return final_readings #, maxval
+
+    def polar2xy(self, lidar):
+            x_lidar = []
+            y_lidar = []
+            min_angle = np.deg2rad(-45)
+            max_angle = np.deg2rad(225)
+            angle = np.linspace(min_angle, max_angle, 1081, endpoint = False)
+            for i in range(0, len(lidar)):
+                x_lidar.append(lidar[i]*math.cos(angle[i]))
+                y_lidar.append(lidar[i]*math.sin(angle[i]))
+                
+            return x_lidar, y_lidar
+
+    def plot_lines(self, xl,yl,t):
+        LW=0.8
+        plt.cla()
+        plt.plot(xl,yl,'g.')
+        plt.xlim(-LW,LW)
+        plt.ylim(-1,5)
+        plt.axis('off')
+        plt.pause(0.1)
+        print(t)
+        # name = '/home/andres/Documents/learning_lidar/images_approach/images/image'+str(t)
+        #plt.savefig(name)
+        plt.savefig(os.getcwd() + '\\assets\\images' + '\\image' + str(t))
+
+if __name__ == '__main__':
+    l2i = lidar2images()
+    for step in range(0,len(l2i.data)):
+        lidar = ((l2i.data[step]).split(","))[7:1088]
+        #print(lidar)
+        lidar_readings = limitLidar(lidar)
+        #print(lidar_readings)
+        x,y = polar2xy(lidar_readings) 
+        #print(x)
+        plot_lines(x,y,step)
