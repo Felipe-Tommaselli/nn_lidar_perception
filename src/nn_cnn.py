40758e49e98e87853c787faac3a2d55c075781e5
Felipe-Tommaselli <f.tommaselli@usp.com>
2023-01-25 20:17:58 -0300

    testando uma resnet aqui, ainda não consegui plotar ela certinho mas tamo indo

--- src/nn_cnn.py
+++ src/nn_cnn.py
@@ -35,11 +35,88 @@ torch.cuda.empty_cache()
 
 from dataloader import *
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
 
-def getData(img_path, csv_path, batch_size=10, num_workers=0):
+class NetworkCNN(nn.Module):
+    
+    def __init__(self, block, num_classes = 4):
+        super(NetworkCNN, self).__init__()
+        
+        # input image: 507x507
+        layers = [2, 2, 2, 2]
+
+        # input image: 507x507
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(1, 64, kernel_size = 3, stride = 2, padding = 1),
+                        nn.BatchNorm2d(64),
+                        nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 1, padding = 1)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * 26 * 26, num_classes)
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        print('shape:', x.shape)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def getData(csv_path, batch_size=5, num_workers=0):
     ''' get images from the folder (assets/images) and return a DataLoader object '''
-    train_data = DataLoader(LidarDatasetCNN(img_path, csv_path, train=True), batch_size=batch_size, shuffle=True,num_workers=num_workers)
-    test_data = DataLoader(LidarDatasetCNN(img_path, csv_path, train=False), batch_size=batch_size, shuffle=True,num_workers=num_workers)
+    train_data = DataLoader(LidarDatasetCNN(csv_path, train=True), batch_size=batch_size, shuffle=True,num_workers=num_workers)
+    test_data = DataLoader(LidarDatasetCNN(csv_path, train=False), batch_size=batch_size, shuffle=True,num_workers=num_workers)
 
     # get one image shape from the train_data
     for i, data in enumerate(train_data):
@@ -47,90 +124,6 @@ def getData(img_path, csv_path, batch_size=10, num_workers=0):
         break
     print('-'*65)
     return train_data, test_data
-
-class NetworkCNN(nn.Module):
-    
-    def __init__(self):
-        super(NetworkCNN, self).__init__()
-
-        super(NetworkCNN, self).__init__()
-        # input image: 507x507
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64)
-        )
-        
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128)
-        )
-        
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256)
-        )
-        
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512)
-        )
-        
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512, 4)
-
-
-    def forward(self, x) -> torch.Tensor:
-        # first convolutional layer
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        # residual block 1
-        residual = x
-        x = self.layer1(x)
-        x += residual  # add the input to the output of the block
-
-        # residual block 2
-        residual = x
-        x = self.layer2(x)
-        print('residual shape: ', residual.shape)
-        print('x shape: ', x.shape)
-        x += residual  # add the input to the output of the block
-
-        # residual block 3
-        residual = x
-        x = self.layer3(x)
-        x += residual  # add the input to the output of the block
-
-        # residual block 4
-        residual = x
-        x = self.layer4(x)
-        x += residual  # add the input to the output of the block
-
-        # average pooling and fully connected layer
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
 
 def fit(model, criterion, optimizer, train_loader, test_loader, num_epochs):
 
@@ -195,6 +188,8 @@ def fit(model, criterion, optimizer, train_loader, test_loader, num_epochs):
 
                     predictions = torch.max(outputs, 1)[1].to(device)
                     predictions_list.append(predictions)
+                    print('predictions:', predictions)
+                    print('labels:', labels)
                     correct += (predictions == labels).sum()
 
                     test_loss += criterion(outputs, labels).item()
@@ -249,7 +244,7 @@ if __name__ == '__main__':
     train_data, test_data = getData(csv_path="~/Documents/IC_NN_Lidar/assets/tags/Label_Data.csv")
 
     # Create the model on GPU if available
-    model = NetworkCNN().to(device)
+    model = NetworkCNN(ResidualBlock).to(device)
     # summary(model, (1, 650, 650))
 
     # loss function for regression
