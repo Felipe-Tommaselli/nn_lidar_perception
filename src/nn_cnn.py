1be231c80df6464e69d38e4e557c604aa777c86b
Felipe-Tommaselli <f.tommaselli@usp.com>
2023-03-11 22:09:03 -0300

    pre treinamento já tá ficando legal, ideal agora é apostar em data augmentantion e depois ir pra as tecnicas avançadas mesmo

--- src/nn_cnn.py
+++ src/nn_cnn.py
@@ -103,7 +103,6 @@ class NetworkCNN(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.avgpool(x)
-        print(x.shape)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
