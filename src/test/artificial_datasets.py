908ac0200423cc30f8f3fd8534886f5d06c83a76
Felipe-Tommaselli <f.tommaselli@usp.br>
2023-06-03 13:00:47 -0300

    rotação funcionando, falta melhorar um pouco a dispersão dos pontos, principalmente usando a gaussiana

--- src/test/artificial_datasets.py
+++ src/test/artificial_datasets.py
@@ -5,100 +5,168 @@ import math
 # Configurações
 image_size = 224  # Tamanho da imagem
 divider = 80  # Espaçamento entre as retas
-pivot = (112, 224)
+pivot = (112, 0)
 
-# Equação da reta para as retas paralelas
-x1 = image_size // 2 - divider // 2  # Coordenada x para a primeira reta
-x2 = image_size // 2 + divider // 2  # Coordenada x para a segunda reta
+# Equação da reta para as retas paralelas VERTICAIS (m = y2 - y1, sendo x2 - x1 = 1 forçado)
+# 
+x1 = image_size // 2 - divider // 2  # Coordenada x para a primeira reta (vertical)
+x2 = image_size // 2 + divider // 2  # Coordenada x para a segunda reta (vertical)
 
+# equação reta vertical (angle = 0): y = mx + b => m = y2 - y1 e b = y1 - (y2 - y1)*x1
+y1_1, y2_1 = 0, 0
+y1_2, y2_2 = image_size, image_size
+m1 = (y1_2 - y1_1)
+m2 = (y2_2 - y2_1)
+b1 = y1_2 - (y1_2 - y1_1)*x1
+b2 = y2_2 - (y2_2 - y2_1)*x2
+
+print(f'equação reta 1: y = {m1}*x + {b1}')
+print(f'equação reta 2: y = {m2}*x + {b2}')
 
 # Gerar coordenadas aleatórias para os pontos
 x_coords = []
 y_coords = []
 
-# Gerar pontos entre as retas
-for _ in range(30):
-    x = np.random.choice([np.random.randint(x1, x1+15), np.random.randint(x2-15, x2)])
-    y = np.random.randint(0, image_size//2)
-    x_coords.append(x)
-    y_coords.append(y)
+# rotacionar as retas
+for angle in range(-30, 30, 9):
 
-# Gerar pontos nos 20 primeiros pixels de fora de cada reta
-for _ in range(90):
-    x = np.random.choice([np.random.randint(x1-15, x1), np.random.randint(x2, x2+15)])
-    y = np.random.randint(0, image_size//2)
-    x_coords.append(x)
-    y_coords.append(y)
+    # Gerar coordenadas aleatórias para os pontos
+    x_coords = []
+    y_coords = []
+    #* ################ ROTATE LINES ################
 
-# Gerar pontos nos 20 primeiros pixels de fora de cada reta
-for _ in range(25):
-    x = np.random.choice([np.random.randint(x1-15, x1), np.random.randint(x2, x2+15)])
-    y = np.random.randint(image_size//2, image_size)
-    x_coords.append(x)
-    y_coords.append(y)
+    # RETA VERTICAL
+    x1 = np.full(image_size, image_size // 2 - divider // 2)
+    x2 = np.full(image_size, image_size // 2 + divider // 2)
 
-# Gerar pontos entre os 20 primeiros pixels e os 40 primeiros pixels
-for _ in range(30):
-    x = np.random.choice([np.random.randint(x1-40, x1-15), np.random.randint(x2+15, x2+40)])
-    y = np.random.randint(0, image_size//3)
-    x_coords.append(x)
-    y_coords.append(y)
+    y1 = range(image_size)
+    y2 = range(image_size)
 
-# Gerar pontos entre os pixels nos pontos mais distantes das retas
-for _ in range(20):
-    x = np.random.choice([np.random.randint(0, 30), np.random.randint(image_size - 30, image_size)])
-    y = np.random.randint(0, image_size//2)
-    x_coords.append(x)
-    y_coords.append(y)
+    # ROTATION POINTS
+    rot_point_np = np.array([pivot[0], pivot[1]])
+    rot_point = (pivot[0], pivot[1])
+
+    # ROTATION MATRIX (
+    rotation_matrix = np.array([[np.cos(np.radians(angle)), np.sin(np.radians(angle)), rot_point_np[0]*(1-np.cos(np.radians(angle)))-rot_point_np[1]*np.sin(np.radians(angle))], 
+                                [-np.sin(np.radians(angle)), np.cos(np.radians(angle)), rot_point_np[1]*(1-np.cos(np.radians(angle)))+rot_point_np[0]*np.sin(np.radians(angle))], 
+                                [0, 0, 1]])
+
+    # add one dim to the points for matrix multiplication
+    points1 = np.stack((x1, y1, np.ones_like(x1)))
+    points2 = np.stack((x2, y2, np.ones_like(x2)))
+
+    # apply transformation
+    transformed_points1 = rotation_matrix @ points1
+    transformed_points2 = rotation_matrix @ points2
+
+    # get the new points
+    x1_rotated = transformed_points1[0]
+    y1_rotated = transformed_points1[1]
+    x2_rotated = transformed_points2[0]
+    y2_rotated = transformed_points2[1]
+
+    # get the new line parameters
+    m1r, b1r = np.polyfit(x1_rotated, y1_rotated, 1)
+    m2r, b2r = np.polyfit(x2_rotated, y2_rotated, 1)
     
+    # new lines 
+    x_line = np.arange(0, image_size, 1)
+    y1_line_rotated = m1r*x_line + b1r
+    y2_line_rotated = m2r*x_line + b2r
 
-# Gerar pontos aleatórios ao longo da imagem
-for _ in range(5):
-    x = np.random.randint(0, image_size)
-    y = np.random.randint(0, image_size)
-    x_coords.append(x)
-    y_coords.append(y)
+    #* ################ GENERATE POINTS ################
 
+    # Gerar pontos entre as retas
+    for _ in range(30):
+        boundary = 15
+        # escolher um y aleatorio entre 0 e 224
+        y = np.random.randint(0, image_size//2)
+        # achar o limiar da reta de boundary para aquele x e da reta normal
+        x1_boundary = (y - b1r) / m1r
+        x2_boundary = (y - b2r) / m2r
+        # fit do x1 e x2 com os boundarys
+        x = np.random.choice([np.random.randint(x1_boundary, x1_boundary + boundary), np.random.randint(x2_boundary - boundary, x2_boundary)])
+        x_coords.append(x)
+        y_coords.append(y)
 
-# Definir coordenadas iniciais das retas
-start_x1 = pivot[0] - 40
-start_y1 = 0
-start_x2 = pivot[0] + 40
-start_y2 = 0
+    # Gerar pontos nos 20 primeiros pixels de fora de cada reta
+    for _ in range(90):
+        boundary = 20
+        # escolher um y aleatorio entre 0 e 224
+        y = np.random.randint(0, image_size//2)
+        # achar o limiar da reta de boundary para aquele x e da reta normal
+        x1_boundary = (y - b1r) / m1r
+        x2_boundary = (y - b2r) / m2r
+        # fit do x1 e x2 com os boundarys
+        x = np.random.choice([np.random.randint(x1_boundary - boundary, x1_boundary), np.random.randint(x2_boundary, x2_boundary + boundary)])
+        x_coords.append(x)
+        y_coords.append(y)
 
-# Função para rotacionar as retas
-def rotate_lines(angle):
-    # Converter ângulo para radianos
-    theta = np.deg2rad(angle)
-    
-    # Calcular seno e cosseno do ângulo
-    sin_theta = np.sin(theta)
-    cos_theta = np.cos(theta)
-    
-    # Rotacionar as coordenadas dos pontos
-    end_x1 = pivot[0] + cos_theta * (start_x1 - pivot[0]) - sin_theta * (start_y1 - pivot[1])
-    end_y1 = pivot[1] + sin_theta * (start_x1 - pivot[0]) + cos_theta * (start_y1 - pivot[1])
-    
-    end_x2 = pivot[0] + cos_theta * (start_x2 - pivot[0]) - sin_theta * (start_y2 - pivot[1])
-    end_y2 = pivot[1] + sin_theta * (start_x2 - pivot[0]) + cos_theta * (start_y2 - pivot[1])
-    
+    # Gerar pontos nos 20 primeiros pixels de fora de cada reta (altura de cima)
+    for _ in range(30):
+        boundary = 10
+        # escolher um y aleatorio entre 0 e 224
+        y = np.random.randint(image_size//2, image_size)
+        # achar o limiar da reta de boundary para aquele x e da reta normal
+        x1_boundary = (y - b1r) / m1r
+        x2_boundary = (y - b2r) / m2r
+        # fit do x1 e x2 com os boundarys
+        x = np.random.choice([np.random.randint(x1_boundary - boundary, x1_boundary), np.random.randint(x2_boundary, x2_boundary + boundary)])
+        x_coords.append(x)
+        y_coords.append(y)
+
+    # Gerar pontos entre os 20 primeiros pixels e os 40 primeiros pixels
+    for _ in range(35):
+        boundary1 = 15
+        boundary2 = 40
+        # escolher um y aleatorio entre 0 e 224
+        y = np.random.randint(0, image_size//3)
+        # achar o limiar da reta de boundary para aquele x e da reta normal
+        x1_boundary = (y - b1r) / m1r
+        x2_boundary = (y - b2r) / m2r
+        # fit do x1 e x2 com os boundarys
+        x = np.random.choice([np.random.randint(x1_boundary - boundary2, x1_boundary - boundary1), np.random.randint(x2_boundary + boundary1, x2_boundary + boundary2)])
+        x_coords.append(x)
+        y_coords.append(y)
+
+    # Gerar pontos entre os pixels nos pontos mais distantes das retas
+    for _ in range(25):
+        x = np.random.choice([np.random.randint(0, 30), np.random.randint(image_size - 30, image_size)])
+        y = np.random.randint(0, image_size//2)
+        x_coords.append(x)
+        y_coords.append(y)
+        
+    # Gerar pontos aleatórios ao longo da imagem
+    for _ in range(5):
+        x = np.random.randint(0, image_size)
+        y = np.random.randint(0, image_size)
+        x_coords.append(x)
+        y_coords.append(y)
+
     # Criar a figura e o eixo
     fig, ax = plt.subplots(figsize=(5, 5))
     ax.set_xlim(0, image_size)
     ax.set_ylim(0, image_size)
     ax.set_title(f'Angle: {angle}°')
     
-    # Desenhar as retas rotacionadas
-    ax.plot([start_x1, end_x1], [start_y1, end_y1], 'r')
-    ax.plot([start_x2, end_x2], [start_y2, end_y2], 'g')
-    
+    # Desenhar as retas rotacionadas (com pontos de início e fim)
+    ax.plot(x_line, y1_line_rotated, 'r')
+    ax.plot(x_line, y2_line_rotated, 'b')
+
+    yline = np.arange(0, image_size, 1)
+    x1line = (yline - b1r) / m1r - 20
+    x2line = (yline - b2r) / m2r + 20
+
+    ax.plot(x1line, yline, 'c')
+    ax.plot(x2line, yline, 'c')
+
     # Desenhar os pontos
     ax.scatter(x_coords, y_coords, s=80, c='black')
     
     # Remover as bordas e ticks dos eixos
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.axis('off')
+    # ax.set_xticks([])
+    # ax.set_yticks([])
+    # ax.axis('off')
     
     # Salvar a imagem
     plt.savefig(f'dataset_image.png', bbox_inches='tight', pad_inches=0, dpi=300)
@@ -108,7 +176,3 @@ def rotate_lines(angle):
     
     plt.close()
 
-
-# Iterar pelos ângulos desejados
-for angle in range(0, 360, 10):
-    rotate_lines(angle)
