96548dc13d330fac22e7b3d45182a2ac37b200b6
Felipe-Tommaselli <f.tommaselli@usp.br>
2023-06-02 23:39:36 -0300

    rotação não está funcionando ainda, as retas não estão paralelas, mas tamo no processo

--- src/test/artificial_datasets.py
+++ src/test/artificial_datasets.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # Configurações
 image_size = 224  # Tamanho da imagem
 divider = 80  # Espaçamento entre as retas
+pivot = (112, 224)
 
 # Equação da reta para as retas paralelas
 x1 = image_size // 2 - divider // 2  # Coordenada x para a primeira reta
 x2 = image_size // 2 + divider // 2  # Coordenada x para a segunda reta
+
 
 # Gerar coordenadas aleatórias para os pontos
 x_coords = []
@@ -56,28 +59,56 @@ for _ in range(5):
     x_coords.append(x)
     y_coords.append(y)
 
-# Criar a figura e o eixo
-fig, ax = plt.subplots(figsize=(5, 5))
-ax.set_xlim(0, image_size)
-ax.set_ylim(0, image_size)
 
-# Desenhar as retas
-ax.plot([x1, x1], [0, image_size], 'r')
-ax.plot([x2, x2], [0, image_size], 'r')
+# Definir coordenadas iniciais das retas
+start_x1 = pivot[0] - 40
+start_y1 = 0
+start_x2 = pivot[0] + 40
+start_y2 = 0
 
-# Desenhar os pontos
-ax.scatter(x_coords, y_coords, s=80, c='black')
+# Função para rotacionar as retas
+def rotate_lines(angle):
+    # Converter ângulo para radianos
+    theta = np.deg2rad(angle)
+    
+    # Calcular seno e cosseno do ângulo
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+    
+    # Rotacionar as coordenadas dos pontos
+    end_x1 = pivot[0] + cos_theta * (start_x1 - pivot[0]) - sin_theta * (start_y1 - pivot[1])
+    end_y1 = pivot[1] + sin_theta * (start_x1 - pivot[0]) + cos_theta * (start_y1 - pivot[1])
+    
+    end_x2 = pivot[0] + cos_theta * (start_x2 - pivot[0]) - sin_theta * (start_y2 - pivot[1])
+    end_y2 = pivot[1] + sin_theta * (start_x2 - pivot[0]) + cos_theta * (start_y2 - pivot[1])
+    
+    # Criar a figura e o eixo
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.set_xlim(0, image_size)
+    ax.set_ylim(0, image_size)
+    ax.set_title(f'Angle: {angle}°')
+    
+    # Desenhar as retas rotacionadas
+    ax.plot([start_x1, end_x1], [start_y1, end_y1], 'r')
+    ax.plot([start_x2, end_x2], [start_y2, end_y2], 'g')
+    
+    # Desenhar os pontos
+    ax.scatter(x_coords, y_coords, s=80, c='black')
+    
+    # Remover as bordas e ticks dos eixos
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.axis('off')
+    
+    # Salvar a imagem
+    plt.savefig(f'dataset_image.png', bbox_inches='tight', pad_inches=0, dpi=300)
+    
+    # Mostrar a imagem na tela
+    plt.show()
+    
+    plt.close()
 
-# Remover as bordas e ticks dos eixos
-ax.set_xticks([])
-ax.set_yticks([])
-ax.axis('off')
 
-
-# Salvar a imagem
-plt.savefig('dataset_image.png', bbox_inches='tight', pad_inches=0, dpi=300)
-
-# Mostrar a imagem na tela
-plt.show()
-
-plt.close()
+# Iterar pelos ângulos desejados
+for angle in range(0, 360, 10):
+    rotate_lines(angle)
