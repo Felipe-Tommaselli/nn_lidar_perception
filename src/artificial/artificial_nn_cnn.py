bb07087e1b32e657d8a1cdd2de48c5b53500e7b1
Felipe-Tommaselli <f.tommaselli@usp.br>
2023-06-10 19:50:43 -0300

    erro de sintaxe

--- src/artificial/artificial_nn_cnn.py
+++ src/artificial/artificial_nn_cnn.py
@@ -326,9 +326,8 @@ if __name__ == '__main__':
     #     nn.Linear(256, 4)
     # )
 
-
     # Carregar o modelo ViT-Lite com pesos pré-treinados
-    model = vits.vit_lite_patch16_224(pretrained=True)
+    model = timm.create_model('vit_lite_patch16_224', pretrained=True)
 
     # Modificar a primeira camada convolucional para aceitar 1 canal de cor
     model.patch_embed.proj = nn.Conv2d(1, model.patch_embed.proj.out_channels,
