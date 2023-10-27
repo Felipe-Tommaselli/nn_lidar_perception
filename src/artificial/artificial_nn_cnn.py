b3abf748fd53e50d87e790f8a8010cd74f802796
Felipe-Tommaselli <f.tommaselli@usp.br>
2023-10-26 23:28:05 -0300

    vou rodar na AWS agora

--- src/artificial/artificial_nn_cnn.py
+++ src/artificial/artificial_nn_cnn.py
@@ -306,8 +306,8 @@ if __name__ == '__main__':
     ############ START ############
     # Set the device to GPU if available
     global device
-    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cpu')
     print('Using {} device'.format(device))
 
     ############ PARAMETERS ############    
