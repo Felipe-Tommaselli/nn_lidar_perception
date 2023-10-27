9dd7c43d7c94793621f841cdc3740b583808f085
Felipe-Tommaselli <f.tommaselli@usp.br>
2023-10-27 18:32:38 -0300

    esqueci um ..

--- src/artificial/artificial_nn_cnn.py
+++ src/artificial/artificial_nn_cnn.py
@@ -339,7 +339,7 @@ if __name__ == '__main__':
     ############ DATA ############
     csv_path = "../../artificial_data/tags/Artificial_Label_Data4.csv"
     # train_path = os.getcwd() + SLASH + 'artificial_data' + SLASH + 'train4' + SLASH
-    train_path = os.path.join(os.getcwd(), '..', 'artificial_data', 'train4')
+    train_path = os.path.join(os.getcwd(), '..', '..', artificial_data', 'train4')
     train_data, val_data = getData(batch_size=batch_size, csv_path=csv_path, train_path=train_path)
 
     ############ MODEL ############
