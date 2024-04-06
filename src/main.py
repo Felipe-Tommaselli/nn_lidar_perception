e1532b98c5f419c94ae6e69d1d535fee7bcdbab0
Felipe-Tommaselli <f.tommaselli@usp.br>
2024-04-06 20:58:44 -0300

    aumentando o lr e buscando soluções mais inovadoras que o fine tuning exaustivo

--- src/main.py
+++ src/main.py
@@ -138,9 +138,9 @@ if __name__ == '__main__':
 
     ############ PARAMETERS ############    
     epochs = 50
-    lr = float(0.009) # TODO: test different learning rates
-    step_size = 8 # TODO: test different step sizes
-    gamma = 0.4
+    lr = float(0.01) # TODO: test different learning rates
+    step_size = 10 # TODO: test different step sizes
+    gamma = 0.5
     batch_size = 140 # 140 AWS
     weight_decay = 0 # L2 regularization
 
